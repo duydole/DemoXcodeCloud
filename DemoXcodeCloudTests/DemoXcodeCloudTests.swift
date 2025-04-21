9738839db9b33642f9cd8ec8cd5f5c8a998f1dd7
Đỗ Lê Duy <duy.do@livepayments.com>
2025-04-21 13:13:29 +0700

    First commit

---
+++ DemoXcodeCloudTests/DemoXcodeCloudTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoXcodeCloudTests.swift
+//  DemoXcodeCloudTests
+//
+//  Created by Đỗ Lê Duy on 21/4/25.
+//
+
+import Testing
+@testable import DemoXcodeCloud
+
+struct DemoXcodeCloudTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
