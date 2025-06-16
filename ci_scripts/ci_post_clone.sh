b1c748e8db0ac9a98ffeeee65f7b411e81de7884
Đỗ Lê Duy <duy.do@livepayments.com>
2025-06-16 15:44:23 +0700

    Update

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,1 @@
+brew install sonar-scanner
--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 set -e
 
-if [ "$CI_ACTION" = "test" ]; then
+xcodebuild test \
+-project DemoXcodeCloud.xcodeproj \
+-scheme DemoXcodeCloud \
+-destination 'platform=iOS Simulator,OS=18.2,name=iPhone 15 Pro' \
+-enableCodeCoverage YES
 
-    if ! command -v slather &> /dev/null; then
-        echo "duydl>> Install slather"
-        bundle install
-        # brew install slather
-    fi
-    
-    slather coverage --sonarqube-xml
-
-    # mkdir -p $CI_DERIVED_DATA_PATH/artifacts
-    # cp -r ./coverage-report $CI_DERIVED_DATA_PATH/artifacts/
-
-    echo "duydl>> Congrat generate report success!"
-else
-    echo "duydl>> Ignore slather, not TEST"
+if ! command -v slather &> /dev/null; then
+    echo "duydl>> Install slather"
+    bundle install
+    # brew install slather
 fi
+
+slather coverage --sonarqube-xml
+
+# mkdir -p $CI_DERIVED_DATA_PATH/artifacts
+# cp -r ./coverage-report $CI_DERIVED_DATA_PATH/artifacts/
+
+echo "duydl>> Congrat generate report success!"
 
 exit 0
