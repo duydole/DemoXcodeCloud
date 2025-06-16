753835cec8fe5a67c011721582819704e7f4206e
Đỗ Lê Duy <duy.do@livepayments.com>
2025-06-16 15:16:56 +0700

    Update

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
-
-# Dừng script nếu có lỗi
 set -e
 
-# Kiểm tra xem Slather đã được cài chưa, nếu chưa thì cài qua Homebrew
-if ! command -v slather &> /dev/null; then
-    echo "Cài đặt Slather..."
-    brew install slather
+if [ "$CI_ACTION" = "test" ]; then
+
+    if ! command -v slather &> /dev/null; then
+        echo "duydl>> Install slather"
+        bundle install
+        # brew install slather
+    fi
+    
+    slather coverage --sonarqube-xml
+
+    # mkdir -p $CI_DERIVED_DATA_PATH/artifacts
+    # cp -r ./coverage-report $CI_DERIVED_DATA_PATH/artifacts/
+
+    echo "duydl>> Congrat generate report success!"
+else
+    echo "duydl>> Ignore slather, not TEST"
 fi
 
-# Chạy Slather để tạo báo cáo test coverage
-echo "Tạo báo cáo test coverage với Slather..."
-slather coverage --sonarqube-xml
-
-# Di chuyển báo cáo vào thư mục artifact (nếu cần tải lên)
-# echo "Sao chép báo cáo vào thư mục artifact..."
-# mkdir -p $CI_DERIVED_DATA_PATH/artifacts
-# cp -r ./coverage-report $CI_DERIVED_DATA_PATH/artifacts/
-
-# Thoát với mã 0 nếu thành công
-echo "Hoàn tất tạo báo cáo test coverage."
 exit 0
