13d549e3dcdbf2306e5a07769b0638c39c9d5d19
Đỗ Lê Duy <duy.do@livepayments.com>
2025-06-19 15:03:02 +0700

    Update

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,31 +1,31 @@
-#!/bin/sh
-set -e
+# #!/bin/sh
+# set -e
 
-echo "duydl>> Danh sách file trong thư mục hiện tại:"
-ls -la
-echo "CI_WORKSPACE: $CI_WORKSPACE"
-echo "CI_DERIVED_DATA_PATH: $CI_DERIVED_DATA_PATH"
+# echo "duydl>> Danh sách file trong thư mục hiện tại:"
+# ls -la
+# echo "CI_WORKSPACE: $CI_WORKSPACE"
+# echo "CI_DERIVED_DATA_PATH: $CI_DERIVED_DATA_PATH"
 
-cd $CI_PRIMARY_REPOSITORY_PATH
+# cd $CI_PRIMARY_REPOSITORY_PATH
 
-xcodebuild test \
--project DemoXcodeCloud.xcodeproj \
--scheme DemoXcodeCloud \
--destination 'platform=iOS Simulator,OS=18.3.1,name=iPhone 16' \
--enableCodeCoverage YES
+# xcodebuild test \
+# -project DemoXcodeCloud.xcodeproj \
+# -scheme DemoXcodeCloud \
+# -destination 'platform=iOS Simulator,OS=18.3.1,name=iPhone 16' \
+# -enableCodeCoverage YES
 
-if ! command -v slather &> /dev/null; then
-    echo "duydl>> Install slather"
-    # bundle install
-    gem install slather
-    #brew install slather
-fi
+# if ! command -v slather &> /dev/null; then
+#     echo "duydl>> Install slather"
+#     # bundle install
+#     gem install slather
+#     #brew install slather
+# fi
 
-slather coverage --sonarqube-xml
+# slather coverage --sonarqube-xml
 
-# mkdir -p $CI_DERIVED_DATA_PATH/artifacts
-# cp -r ./coverage-report $CI_DERIVED_DATA_PATH/artifacts/
+# # mkdir -p $CI_DERIVED_DATA_PATH/artifacts
+# # cp -r ./coverage-report $CI_DERIVED_DATA_PATH/artifacts/
 
-echo "duydl>> Congrat generate report success!"
+# echo "duydl>> Congrat generate report success!"
 
-exit 0
+# exit 0
