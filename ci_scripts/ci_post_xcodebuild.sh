aa9e8c0a292902c009f3425fb6c565b1c25af9c7
Đỗ Lê Duy <duy.do@livepayments.com>
2025-06-22 13:30:36 +0700

    Update

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -17,18 +17,5 @@ PRODUCT_NAME="DemoXcodeCloud"
 PROJECT_NAME="DemoXcodeCloud"
 
 fastlane test
-# sonar-scanner
+sonar-scanner
 
-# # Clean, build, and test project
-# xcodebuild \
-# -project "${PROJECT_NAME}.xcodeproj" \
-# -destination 'platform=iOS Simulator,name=iPhone 16,OS=18.3.1' \
-# -scheme "${SCHEME}" \
-# -derivedDataPath DerivedData/ \
-# -enableCodeCoverage YES \
-# -resultBundlePath DerivedData/Logs/Test/ResultBundle.xcresult \
-# clean build test
-
-# # Run slather
-# echo "Start run slather"
-# fastlane slather
